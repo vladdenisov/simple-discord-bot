ff89319cd0150908ebef4f7707817cf9a0ac9115
Vlad Denisov <ipovlad.yt@gmail.com>
2019-05-07 15:43:30 +0300

    Add Weather Command

---
+++ commands/weather.js
@@ -0,0 +1,20 @@
+const {
+    api_w
+} = require('../json/config.json');
+const request = require('request');
+const {
+    Attachment
+} = require('discord.js');
+
+exports.run = (client, message, args) => {
+    let zip = parseInt(args[0]);
+    let cc = args[1];
+    request(`https://api.weatherbit.io/v2.0/current?postal_code=${zip}&country=${cc.toUpperCase()}&key=${api_w}`, function (error, response, body) {
+        body = JSON.parse(body);
+        const img_t = new Attachment(`https://www.weatherbit.io/static/img/icons/${body.data[0].weather.icon}.png`);
+        const desc = body.data[0].weather.description;
+        const tempt = body.data[0].temp;
+        const app_temp = body.data[0].app_temp;
+        message.channel.send(`It's ${tempt}°С there. Feels like ${app_temp}°C. And it's ${desc}!`, img_t);
+    });
+}
